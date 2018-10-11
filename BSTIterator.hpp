3a50d30fdf7c0f951c5ce71c45dab26af7558520
AllenLuo9566 <33334527+AllenLuo9566@users.noreply.github.com>
2018-10-10 21:03:56 -0700

    Update BSTIterator.hpp

--- BSTIterator.hpp
+++ BSTIterator.hpp
@@ -1,1 +1,42 @@
-keke
+#ifndef BSTITERATOR_HPP
+#define BSTITERATOR_HPP
+#include <iterator>
+#include <list>
+#include "BSTNode.hpp"
+using namespace std;
+
+template <typename Data>
+class BSTIterator : public iterator<input_iterator_tag, Data> {
+   private:
+    BSTNode<Data>* curr;
+
+   public:
+    /** Constructor.  Use the argument to initialize the current BSTNode
+     *  in this BSTIterator.
+     */ // TODO
+    BSTIterator(BSTNode<Data>* curr) {}
+
+    /** Dereference operator. */
+    Data operator*() const { return curr->data; }
+
+    /** Pre-increment operator. */
+    BSTIterator<Data>& operator++() {
+        curr = curr->successor();
+        return *this;
+    }
+
+    /** Post-increment operator. */
+    BSTIterator<Data> operator++(int) {
+        BSTIterator before = BSTIterator(curr);
+        ++(*this);
+        return before;
+    }
+
+    /** Equality test operator. */  // TODO
+    bool operator==(BSTIterator<Data> const& other) const {}
+
+    /** Inequality test operator. */  // TODO
+    bool operator!=(BSTIterator<Data> const& other) const {}
+};
+
+#endif  // BSTITERATOR_HPP
